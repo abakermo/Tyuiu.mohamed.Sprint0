212122e3951e83931450a1c3c046399d67b03eb4
tyuiu.mohamed@outlook.com <tyuiu.mohamed@outlook.com>
2024-09-28 12:36:59 +0300

    Task

---
+++ Tyuiu.mohamed.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.mohamed.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello world");
+            string name = "mohammed";
+            int age = 22;
+            Console.WriteLine("hello my name is " + name + " iam " + age + " old");              
+       
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
